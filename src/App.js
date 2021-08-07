f86f26ba1cf324d3fc7f7e8e48107a5d67272a6c
HunCage <gkipeter@gmail.com>
2021-08-07 21:55:43 +0200

    app initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+    const [km, setKm] = useState(0);
+
+    const handleChange = (event) => {
+      setKm(event.target.value);
+    }
+
+    const convert = (km) => {
+      return (km / 1.609).toFixed(4);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="header">Convert Km to Miles</p>
       </header>
+      <main className="content">
+          <input 
+            type="text" 
+            value={km} 
+            onChange={handleChange} 
+          />
+          <p className="result"><span className="km">{km} km</span> is <span className="miles">{convert(km)} miles</span> </p>
+      </main>
     </div>
   );
 }
